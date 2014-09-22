16866bdbfa63884af9d63b4974206f3df3e3b4c5
TheoWinter <theo.winter@gmail.com>
2014-09-22 16:23:14 +0200

    added Counter class for exercise 02

---
+++ Ex02/2/CounterLib/CounterLib/Counter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSRCounter
+{
+    public class Counter
+    {
+        private int count;
+        Counter()
+        {
+            count = 0;
+        }
+        Counter(int val)
+        {
+            count = val;
+        }
+
+        public int Inc()
+        {
+            return ++Count;
+        }
+        public int Dec()
+        {
+            return --Count;
+        }
+        public void Reset()
+        {
+            Count = 0;
+        }
+        public void Reset(int val)
+        {
+            Count = val;
+        }
+        public int Count
+        {
+            get{return count;}
+            set{count=value;}
+        }
+    }
+}
