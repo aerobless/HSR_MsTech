b85e739cc8dc402c54a78e9e52adffec01d388f1
TheoWinter <theo.winter@gmail.com>
2014-10-06 16:09:44 +0200

    solved exercise 3.1

--- Ex03/3.1/Fibonacci.cs
+++ Ex03/3.1/Fibonacci.cs
@@ -1,8 +1,26 @@
-using System;
 using System.Collections.Generic;
+using System;
 
-public class Math
+public class Math2
 {
+
+    public static IEnumerable<int> Fibonacci(int lastNumber)
+    {
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i < lastNumber; i++)
+        {
+            if (i == 0)
+            {
+                yield return 0;
+            }
+
+            int temp = a;
+            a = b;
+            b = temp + b;
+            yield return a;
+        }
+    }
    
 }
 class Test
@@ -10,12 +28,12 @@ class Test
     public static void Main()
     {
         // Display powers of 2 up to the exponent 8: 
-        foreach (int i in Math.Fibonacci(8))
+        foreach (int i in Math2.Fibonacci(30))
         {
-            Console.WriteLine("{0} ", i);
+            System.Console.WriteLine("{0} ", i);
         }
 
-        Console.WriteLine("Press Enter to Exit");
-        Console.ReadLine();
+        System.Console.WriteLine("Press Enter to Exit");
+        System.Console.ReadLine();
     }
 }
