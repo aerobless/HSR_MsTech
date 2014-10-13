1539b3f36a2fafcc030b17fc960288d25167d363
TheoWinter <theo.winter@gmail.com>
2014-10-13 15:04:20 +0200

    solved exercise 1.3 w4

--- Ex04/1.3/Extensions.cs
+++ Ex04/1.3/Extensions.cs
@@ -5,8 +5,17 @@ namespace ExtensionMethods
 {
     public static class Extensions
     {
-        //ForEachElem hier als Extension-Method implementieren
-        //
+        /* 
+         * The method has to receive the object from where we're calling first and the
+         * action second.
+         */
+        public static void ForEachElem(this string[] input, Action<string> action)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                action(input[i]);
+            }
+        }
     }
 }
 
