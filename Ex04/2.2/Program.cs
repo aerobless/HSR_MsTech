b124b50f5e1cdbf916e17c87e73fea3c11fd6c3a
TheoWinter <theo.winter@gmail.com>
2014-10-20 10:33:09 +0200

    initial commit exercises 5 week 6

--- Ex04/2.2/Program.cs
+++ Ex04/2.2/Program.cs
@@ -13,8 +13,22 @@ namespace LambdaExpression2
 
             Func<int, bool> f;
 
-            Console.WriteLine("f(1) = {0}", f(1));
-            Console.WriteLine("f(2) = {0}", f(2));
+            Expression<Func<int, bool>> f1 = n => n < 5;
+            PrintExpressionTree(filter);
+
+          ///  Console.WriteLine("f(1) = {0}", f(1));
+           // Console.WriteLine("f(2) = {0}", f(2));
+
+            Console.ReadLine();
+        }
+
+        private static void PrintExpressionTree(Expression<Func<int, bool>> filter)
+        {
+            BinaryExpression body = (BinaryExpression)filter.Body;
+            ParameterExpression left = (ParameterExpression)body.Left;
+            ConstantExpression right = (ConstantExpression)body.Right;
+            Console.WriteLine("{0} {1} {2}",
+            left.Name, body.NodeType, right.Value);
         }
     }
 }
