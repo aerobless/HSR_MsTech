de1fc6c1830f7b84bcda068838ccb236ac843a8c
TheoWinter <theo.winter@gmail.com>
2014-11-03 20:46:22 +0100

    added examples for optionals & named params

--- Scratchpad/CSharpScratchpad/Parameters.cs
+++ Scratchpad/CSharpScratchpad/Parameters.cs
@@ -10,7 +10,7 @@ namespace CSharpScratchpad
 
 		public void Run ()
 		{
-			//Passing by value:
+			//Passing by value: (normal case)
 			int someValue = 3;
 			passByValue (someValue);
 			Console.Out.WriteLine ("Original 3 => " + someValue);
@@ -22,8 +22,17 @@ namespace CSharpScratchpad
 
 			//The out modifier:
 			string result, result2;
-			outModifierExample ("foo", out result, out result2);
+			outModifierExample ("foo", out result, out result2); //Important: "out", otherwise compiletime error.
 			Console.Out.WriteLine (result + "  " + result2);
+
+			//Optional parameter:
+			optionalParameter (34);
+			optionalParameter ();
+
+			//Named parameters:
+			namedParameter ();
+			namedParameter (y: 4, x: 2);
+			namedParameter (z: 500);
 		}
 
 		private void passByValue (int x)
@@ -42,6 +51,20 @@ namespace CSharpScratchpad
 		{
 			result = test + "bar1";
 			result2 = test + "bar2";
+		}
+
+		private void optionalParameter (int x = 24)
+		{
+			if (x == 24) {
+				Console.Out.WriteLine ("Default param 24 used.");
+			} else {
+				Console.Out.WriteLine ("Optional param " + x + " used.");
+			}
+		}
+
+		private void namedParameter (int x = 1, int y = 2, int z = 3)
+		{
+			Console.Out.WriteLine ("X:" + x + " Y:" + y + " Z:" + z);
 		}
 	}
 }
