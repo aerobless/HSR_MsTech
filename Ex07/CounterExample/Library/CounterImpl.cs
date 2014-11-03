8fd5a394c4ae4282e66f8f422b85f3ac3a8209dc
TheoWinter <theo.winter@gmail.com>
2014-11-03 15:53:49 +0100

    created CounterImpl

---
+++ Ex07/CounterExample/Library/CounterImpl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Library
+{
+    public class CounterImpl : ICounter
+    {
+        public event CountChangedEventHandler CountChanged;
+
+        public int Count {
+            get { return Count; }
+            set { Count = value; OnCounterChanged(new CountChangedEventArgs(value, Count)); }
+        }
+
+        public void Increment()
+        {
+            ++Count;
+        }
+        public void Decrement()
+        {
+            --Count;
+        }
+        public void Reset()
+        {
+            Count = 0;
+        }
+
+        protected void OnCounterChanged(CountChangedEventArgs eventArgs)
+        {
+            if (CountChanged != null)
+            {
+                CountChanged(this, eventArgs);
+            }
+        }
+    }
+}
