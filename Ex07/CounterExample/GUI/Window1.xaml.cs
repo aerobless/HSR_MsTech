79a3564dad4d74fab0128707e0b3d1d46d950617
TheoWinter <theo.winter@gmail.com>
2014-11-03 16:33:32 +0100

    sync

--- Ex07/CounterExample/GUI/Window1.xaml.cs
+++ Ex07/CounterExample/GUI/Window1.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Library;
 
 namespace GUI
 {
@@ -7,9 +8,32 @@ namespace GUI
     /// </summary>
     public partial class Window1 : Window
     {
+        private ICounter counter;
+
         public Window1()
         {
             InitializeComponent();
+            counter = new CounterImpl();
+        }
+
+        private void button_decrement(object sender, RoutedEventArgs e)
+        {
+            counter.Decrement();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            counter.Increment();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            counter.Reset();
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
