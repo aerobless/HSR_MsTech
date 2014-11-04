233df9a7510625950db90cb48c4c0bb0520ca07b
TheoWinter <theo.winter@gmail.com>
2014-11-04 10:04:01 +0100

    goto example from yesterday that I forgot to commit

---
+++ Scratchpad/CSharpScratchpad/Various.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharpScratchpad
+{
+	public class Various
+	{
+		public Various ()
+		{
+		}
+
+		public void Run ()
+		{
+			//Why? I just wanted to use goto once in my live!
+			gotoExample ();
+
+
+		}
+
+		static void gotoExample ()
+		{
+			int i = 0;
+			gotoMarker:
+			if (i == 0) {
+				goto gotoMarker;
+			} else {
+				Console.Out.WriteLine ("omg!");
+			}
+		}
+	}
+}
+
